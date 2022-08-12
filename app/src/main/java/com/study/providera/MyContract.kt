a67bac70551de7021f5ec1471c4894814b447fae
eunseop.im <eunseop.im@samsung.com>
2022-08-12 16:11:41 +0900

    - MyContract 수정

--- app/src/main/java/com/study/providera/MyContract.kt
+++ app/src/main/java/com/study/providera/MyContract.kt
@@ -6,6 +6,6 @@ object MyContract {
 
     const val TABLE_NAME = "item"
     const val AUTHORITY = "com.study.providera.MyContentProvider"
-    const val URL = "content://$AUTHORITY/$TABLE_NAME"
-    val CONTENT_URI: Uri = Uri.parse(URL)
+    const val URI_STRING = "content://$AUTHORITY/$TABLE_NAME"
+    val CONTENT_URI: Uri = Uri.parse(URI_STRING)
 }
